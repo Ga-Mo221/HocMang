558cca40d483d82ca75c3c403e22bfaee85cfd57
Ga Mo <oni221ok@gmail.com>
2025-02-26 20:39:10 +0700

    cap nhat moi

---
+++ bai_lab3/UDPSocket/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UDPSocket
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            UDPSocket s = new UDPSocket();
+            s.Server("127.0.0.1", 33333);
+            UDPSocket c = new UDPSocket();
+            c.Client("127.0.0.1", 33333);
+            c.Send("UDP TEST VTCA!");
+
+            Console.ReadKey();
+        }
+    }
+}
